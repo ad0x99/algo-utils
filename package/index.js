83900ac677300c1ebe09a86ed63afa12046a59e6
ad0x99 <50711511+ad0x99@users.noreply.github.com>
2024-01-08 15:50:02 +0700

    chore: centralize imports and exports

---
+++ package/index.js
@@ -0,0 +1,49 @@
+/**
+ * Data Structure Imports/Exports
+ */
+import { SinglyLinkedList } from './data-structures/linked-lists/singly-linked-list/singly-linked-list';
+import { DoublyLinkedList } from './data-structures/linked-lists/doubly-linked-list/doubly-linked-list';
+import { Queue } from './data-structures/queues/queue';
+import { Stack } from './data-structures/stack/stack';
+
+import { PriorityQueue } from './algorithms/binary-heap/priority-queue';
+import { MaxBinaryHeap } from './algorithms/binary-heap/max-binary-heap';
+import { BinarySearchTree } from './algorithms/binary-search-trees/binary-search-tree';
+import { Graph } from './algorithms/graphs/graphs';
+import { HashTable } from './algorithms/hash-tables/hash-tables';
+import { WeightedGraph } from './algorithms/dijkstras-algorithm/dijkastras-algorithm';
+
+/**
+ * Searching Algorithms Imports/Exports
+ */
+
+import { binarySearch } from './algorithms/searching-algorithms/binary-search';
+
+/**
+ * Sorting Algorithms Imports/Exports
+ */
+import { bubbleSort } from './algorithms/sorting-algorithms/bubble-sort/bubble-sort';
+import { insertionSort } from './algorithms/sorting-algorithms/insertion-sort/insertion-sort';
+import { mergeSort } from './algorithms/sorting-algorithms/merge-sort/merge-sort';
+import { quickSort } from './algorithms/sorting-algorithms/quick-sort/quick-sort';
+import { radixSort } from './algorithms/sorting-algorithms/radix-sort/radix-sort';
+import { selectionSort } from './algorithms/sorting-algorithms/selection-sort/selection-sort';
+
+export {
+  SinglyLinkedList,
+  DoublyLinkedList,
+  PriorityQueue,
+  Queue,
+  Stack,
+  MaxBinaryHeap,
+  BinarySearchTree,
+  Graph,
+  HashTable,
+  WeightedGraph,
+  bubbleSort,
+  insertionSort,
+  mergeSort,
+  quickSort,
+  radixSort,
+  selectionSort,
+};
